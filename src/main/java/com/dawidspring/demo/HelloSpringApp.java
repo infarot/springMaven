760f69b5e9ce0f50c59aac8538f73cdd96ce7561
Dawid <infarot@gmail.com>
2018-10-27 12:31:27 +0200

    Configuration of maven

--- src/main/java/com/dawidspring/demo/HelloSpringApp.java
+++ src/main/java/com/dawidspring/demo/HelloSpringApp.java
@@ -1,12 +1,12 @@
 package com.dawidspring.demo;
 
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloSpringApp {
     public static void main(String[] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         CricketCoach theCoach = context.getBean("myCricketCoach",CricketCoach.class);
-
         System.out.println(theCoach.getFortune());
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getEmailAddress());
